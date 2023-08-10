028b6801f6e4c5a366ed7964adf07b99a325e52a
AST <tejasimma36@gmail.com>
2023-08-10 18:21:17 +0530

    adminlogin

--- clientside/src/admin_components/credentials/login/adminlogin.js
+++ clientside/src/admin_components/credentials/login/adminlogin.js
@@ -45,9 +45,9 @@ export const Adminlogin=()=>
         }
 
     }
-    const Regi=async()=>{
-        nav("/adminregister")
-    }
+    // const Regi=async()=>{
+    //     nav("/adminregister")
+    // }
     const Upadate=async()=>{
         nav("/adminupdate")
     }
@@ -79,9 +79,9 @@ export const Adminlogin=()=>
                 </td>
             </tr>
             <tr >
-                <td >
+                {/* <td >
                     <button style={{width:'20vh'}} onClick={Regi}><b>New account</b></button>
-                </td>
+                </td> */}
                 <td>
                     <button onClick={Upadate}><b>Forgot password</b></button>
                 </td>
