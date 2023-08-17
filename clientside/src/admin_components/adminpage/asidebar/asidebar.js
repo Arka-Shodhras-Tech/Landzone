916ae9f2f79a08f1d9445a3355ef8bbe5094fa24
AST <tejasimma36@gmail.com>
2023-08-17 23:24:29 +0530

    asidebar

--- clientside/src/admin_components/adminpage/asidebar/asidebar.js
+++ clientside/src/admin_components/adminpage/asidebar/asidebar.js
@@ -16,7 +16,7 @@ export const Comp=()=>
                             <NavLink to='/dashboard' className="asidebtn"  activeClassName='active'><b>Dashboard</b></NavLink>
                             <NavLink to='/usermanage' className="asidebtn" activeClassName="active"><b>User Management</b></NavLink>
                             {
-                                localStorage.adminmail!==''?<b></b>:<NavLink to='/adminusermanage' className="asidebtn" activeClassName="active"><b>Admin User Management</b></NavLink>
+                                localStorage.mainadmin===''?<br/>:<NavLink to='/adminusermanage' className="asidebtn" activeClassName="active"><b>Admin User Management</b></NavLink>
                             }
                             <NavLink to='/landmanage' className="asidebtn" activeClassName="active"><b>Land Management</b></NavLink>
                             <NavLink to='/unitmanage' className="asidebtn" activeClassName="active"><b>Unit Request Management</b></NavLink>
